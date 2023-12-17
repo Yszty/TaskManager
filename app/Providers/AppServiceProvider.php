66fd53e08babf1ea3a157c02d4475dc2fe2209f3
Yszty <m.michalski@o2.pl>
2023-12-17 22:26:05 +0100

    Bind task services

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Interfaces\TaskServiceInterface;
+use App\Services\Interfaces\UserServiceInterface;
+use App\Services\TaskService;
+use App\Services\UserService;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * All the container singletons that should be registered.
+     *
+     * @var array
+     */
+    public array $singletons = [
+        TaskServiceInterface::class => TaskService::class,
+    ];
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
